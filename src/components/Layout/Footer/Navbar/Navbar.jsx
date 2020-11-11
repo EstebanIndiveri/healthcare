cc8d54782a0a959551da6d231952f7b94eb65fe4
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-11-10 21:16:44 -0300

    base work fix

--- src/components/Layout/Footer/Navbar/Navbar.jsx
+++ src/components/Layout/Footer/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navb = () => {
     return ( 
               <div>
                 <Navbar color="light" light expand="md">
-                  <NavbarBrand href="/">reactstrap</NavbarBrand>
+                  <NavbarBrand><Link className="firmaLogo" to="/">Dr.Indiveri</Link></NavbarBrand>
                   <NavbarToggler onClick={toggle} />
                   <Collapse isOpen={isOpen} navbar>
                     <Nav className="mr-auto" navbar>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 const Home = () => {
-    return ( <div>estamos en la gome</div> );
+    return ( 
+        <Container>
+        <Row>
+        <Col xs="6" md="6" lg="6">
+          <div>como va ese readerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrryyyraaaa</div>
+        </Col>
+        <Col xs="6" md="6" lg="6" >todo piola mi reyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy</Col>
+      </Row>
+        </Container>
+        );
 }
  
 export default Home;
